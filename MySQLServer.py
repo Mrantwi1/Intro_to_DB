93b97e20f16e8933e1a8abdbe2a3a242677db4a3
Mrantwi1 <kwameantwiboasiako020@gmail.com>
2025-10-24 15:57:52 +0000

    Fix: use 'except mysql.connector.Error' for ALX checker

--- MySQLServer.py
+++ MySQLServer.py
@@ -5,16 +5,15 @@ If it already exists, the script will not fail.
 """
 
 import mysql.connector
-from mysql.connector import Error
 
 def create_database():
     connection = None
     try:
-        # Connect to MySQL Server (edit user/password if needed)
+        # Connect to MySQL Server (update password if needed)
         connection = mysql.connector.connect(
             host='localhost',
             user='root',
-            password='#Mrantwi&kwame1'  # <-- change this if your MySQL root user has a password
+            password='#Mrantwi&kwame1'
         )
 
         if connection.is_connected():
@@ -22,7 +21,7 @@ def create_database():
             cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store;")
             print("Database 'alx_book_store' created successfully!")
 
-    except Error as e:
+    except mysql.connector.Error as e:
         print(f"Error: {e}")
 
     finally:
@@ -32,3 +31,4 @@ def create_database():
 
 if __name__ == "__main__":
     create_database()
+
