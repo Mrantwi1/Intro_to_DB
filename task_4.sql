cc14792ce9be0a35b58e1d78b0ff9c6d06e6e6f6
Mrantwi1 <kwameantwiboasiako020@gmail.com>
2025-10-24 16:35:17 +0000

    Fix filename and table name in task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,8 +1,10 @@
--- Switch to the alx_book_store database
 USE alx_book_store;
 
--- Print the full description of the 'books' table
-SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
+SELECT COLUMN_NAME,
+       COLUMN_TYPE,
+       IS_NULLABLE,
+       COLUMN_KEY,
+       COLUMN_DEFAULT,
+       EXTRA
 FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_NAME = 'books'
-  AND TABLE_SCHEMA = 'alx_book_store';
+WHERE TABLE_NAME = 'books';
